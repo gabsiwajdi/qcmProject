8051cf03f91d93cc77d10433e433ac2b805a7ece
infonet1001 <infonet1001@gmail.com>
2023-11-27 17:11:00 +0100

    last commit

--- src/app/doctor/components/students/students.component.ts
+++ src/app/doctor/components/students/students.component.ts
@@ -10,8 +10,9 @@ export class StudentsComponent implements OnInit {
   listStudents: any[] = [];
   dataSource: any;
   displayedColumns: any;
+  dataTable: any;
   constructor(private service: AuthService) {
-    this.displayedColumns = ['id', 'name', 'matiere', 'resultat'];
+    this.displayedColumns = ['userId', 'name', 'matiere', 'resultat'];
   }
 
   ngOnInit(): void {
@@ -19,9 +20,39 @@ export class StudentsComponent implements OnInit {
   }
 
   getAllStudenets() {
+    // en va retourne une array of object {name,subjects,degree} l'objet doit etre stocke dans "dataSource"
     this.service.getAllStudents('students').subscribe((res: any) => {
-      this.listStudents = res;
-      console.log(this.listStudents);
+      this.dataSource = res.map((student: any) => {
+        if (student?.subjects) {
+          return student?.subjects?.map((sub: any) => {
+            return {
+              name: student.userName,
+              subjects: sub.name,
+              degree: sub.degree,
+            };
+          });
+        } else {
+          return [
+            {
+              name: student.userName,
+              subjects: '-',
+              degree: '-',
+            },
+          ];
+        }
+      });
+      console.log(this.dataSource);
+      this.dataTable = [];
+      this.dataSource.forEach((item: any) => {
+        item.forEach((subItem: any) => {
+          this.dataTable.push({
+            name: subItem.name,
+            subjects: subItem.subjects,
+            degree: subItem.degree,
+          });
+        });
+      });
+      console.log('consoleee', this.dataTable);
     });
   }
 }
